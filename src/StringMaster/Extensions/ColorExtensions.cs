89555fd7a5e0fd95ae15406a08f3d599df45ce55
Scott Whitney <79826944+puppetsw@users.noreply.github.com>
2022-09-17 18:52:36 +0930

    Fixed ColorExtension method

--- src/StringMaster/Extensions/ColorExtensions.cs
+++ src/StringMaster/Extensions/ColorExtensions.cs
@@ -7,15 +7,14 @@ public static class ColorExtensions
 {
     public static Color ToColor(this AcadColor acadColor)
     {
-        if (acadColor.ColorIndex is not null)
-        {
-            if (acadColor.IsByLayer)
-                return Color.FromColorIndex(ColorMethod.ByLayer, 256);
-            if (acadColor.IsByBlock)
-                return Color.FromColorIndex(ColorMethod.ByBlock, (short)acadColor.ColorIndex);
+        if (acadColor.IsByLayer)
+            return Color.FromColorIndex(ColorMethod.ByLayer, 256);
 
+        if (acadColor.IsByBlock)
+            return Color.FromColorIndex(ColorMethod.ByBlock, 0);
+
+        if (acadColor.ColorIndex is not null)
             return Color.FromColorIndex(ColorMethod.ByAci, (short)acadColor.ColorIndex);
-        }
 
         return Color.FromRgb(acadColor.Red, acadColor.Green, acadColor.Blue);
     }
